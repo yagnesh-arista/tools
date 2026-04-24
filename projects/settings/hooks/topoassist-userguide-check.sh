245c93ae1ed7dcdbaa4bffbe548a0cc70809a5b3
yagnesh <yagnesh@users.noreply.github.com>
2026-04-24 12:30:49 +0530

    settings: auto-sync topoassist-userguide-check.sh

--- projects/settings/hooks/topoassist-userguide-check.sh
+++ projects/settings/hooks/topoassist-userguide-check.sh
@@ -6,9 +6,6 @@ f=$(echo "$input" | jq -r '.tool_input.file_path // ""')
 
 case "$f" in
   */topoassist/Code.gs|\
-  */topoassist/Sidebar.html|\
-  */topoassist/Sidebar-js.html|\
-  */topoassist/SheetAssistPanel.html|\
   */topoassist/device_bridge.py)
     echo '{"hookSpecificOutput":{"hookEventName":"PostToolUse","additionalContext":"[USERGUIDE] Check if UserGuide.html needs updating for this change."}}'
     ;;
